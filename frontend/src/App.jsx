deeaca9c3df4a981cf18ff98cf90052f0d69f4f5
Raman Sharma <rs9463194297@gmail.com>
2022-11-12 17:04:06 +0530

    added video audio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import io from "socket.io-client";
+import Peer from "peerjs";
 
 import "./App.css";
 
@@ -18,15 +19,66 @@ const connectionOptions = {
 
 const socket = io(server, connectionOptions);
 
+const myPeer = new Peer(undefined, {
+  host: "localhost:5000",
+  port: "3001",
+});
+
 const App = () => {
   const [user, setUser] = useState(null);
   const [users, setUsers] = useState([]);
+  const [peers, setPeers] = useState({});
+  const [openVideo, setOpenVideo] = useState(false);
+
+  const videoGrid = useRef(null);
+
+  const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+      video.play();
+    });
+    videoGrid.current.append(video);
+  };
+
+  const connectToNewUser = (userId, stream) => {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement("video");
+    video.title = users.find((user) => user.id === userId).name;
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+    call.on("close", () => {
+      video.remove();
+    });
+
+    peers[socket.id] = call;
+  };
 
   useEffect(() => {
+    if (user) {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: true,
+        })
+        .then((stream) => {
+          const myVideo = document.createElement("video");
+          addVideoStream(myVideo, stream);
+
+          myPeer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+              addVideoStream(video, userVideoStream);
+            });
+          });
+        });
+    }
     socket.on("userIsJoined", (data) => {
       if (data.success) {
         console.log("userJoined");
         setUsers(data.users);
+        connectToNewUser(socket.id, stream);
       } else {
         console.log("userJoined error");
       }
@@ -44,8 +96,9 @@ const App = () => {
     socket.on("userLeftMessageBroadcasted", (data) => {
       console.log(`${data} left the room`);
       toast.info(`${data} left the room`);
+      if (peers[socket.id]) peers[socket.id].close();
     });
-  }, []);
+  }, [users]);
 
   const uuid = () => {
     let S4 = () => {
@@ -77,7 +130,36 @@ const App = () => {
         />
         <Route
           path="/:roomId"
-          element={<RoomPage user={user} socket={socket} users={users} />}
+          element={
+            <>
+              <button
+                onClick={() => setOpenVideo(!openVideo)}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  zIndex: "100",
+                  backgroundColor: "white",
+                  border: "none",
+                  padding: "10px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+              >
+                Open Video
+              </button>
+              {openVideo && (
+                <div
+                  className="video-grid h-100 position-fixed top-0 end-0"
+                  style={{
+                    zIndex: 1000,
+                  }}
+                  ref={videoGrid}
+                ></div>
+              )}
+              <RoomPage user={user} socket={socket} users={users} />
+            </>
+          }
         />
       </Routes>
     </div>
